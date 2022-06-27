78590e1984ffe4ea92b417b3ffbc2283b3aeed5d
matdexir <matthieudesir21@gmail.com>
2022-06-27 16:10:31 +0800

    First commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="{{ asset('css/app.css')}}" rel="stylesheet">
+
+  <title>Kat.js, the all-in-one framework you need</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <bold>Hello World</bold>
+      </div>
+      <div class="col-md-6">Hello World</div>
+    </div>
+  </div>
+</body>
+
+</html>
