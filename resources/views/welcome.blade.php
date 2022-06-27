ce676e843153049c4e776db88cc3664e5c977e3f
matdexir <matthieudesir21@gmail.com>
2022-06-27 17:04:04 +0800

    Feature: Added a basic landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,12 +10,27 @@
 </head>
 
 <body>
+  <nav class="navbar">
+    <a class="navbar-brand" href="#">
+      <img src="/assets/kat-light.png" width="30" height="30" id="logo">
+      Kat.js
+    </a>
+  </nav>
   <div class="container">
     <div class="row">
       <div class="col-md-6">
-        <bold>Hello World</bold>
+        <div class="m-5 p-3">
+          <h1>The all-in-one framework for web5.0</h1>
+          <div class="mt-5">
+            <button type="button" class="btn btn-primary btn-lg" style="border-radius: 23px;">Learn more</button>
+          </div>
+        </div>
       </div>
-      <div class="col-md-6">Hello World</div>
+      <div class="col-md-6">
+        <div class="p-5">
+          <img src="/assets/kat-light.png" width="300" height="300" style="border-radius: 20%; box-shadow: -20px 10px 30px #20FC8F;">
+        </div>
+      </div>
     </div>
   </div>
 </body>
